88f0eb47aa56b91354ecfc4761fc286274df3c09
Daniel Jones <daniel.jones2000@gmail.com>
2020-02-06 15:37:39 -0800

    created all the rooms

--- util/room_creator.py
+++ util/room_creator.py
@@ -23,8 +23,8 @@ class World:
         direction = 1 #1:east, -1:west
         # While there are rooms to be created...
         previous_room = None
-        room_titles = ["Cave", "Church", "Outside CaveEnterance","Foyer", "Narrow Passage","Grand Overlook","Treasure Chamber","Maids Chamber" ]
-        room_descriptions = ["North of you, the cave mount beckons", "Dim light filters in from the south. Dusty passages run north and east.","You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.","North of you, the cave mount beckons.", "Dim light filters in from the south. Dusty passages run north and east.", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", "The narrow passage bends here from west to north. The smell of gold permeates the air.","Dark room full of old antiquities"]  
+        room_titles = ["Cave", "Church", "Outside CaveEnterance","Foyer", "Narrow Passage","Grand Overlook","Treasure Chamber","Maids Chamber","King's Dining Room", "Courtyard","Kitchen","Guest Room","Grand Room","Safe Room","Guards Chamber","Buttery","Library","Icehouse", "The Dungeion","The Gatehouse", "Place of Arms", "The Gallery", "North Tower Room" , "Stable House", "Barbican"]
+        room_descriptions = ["North of you, the cave mount beckons", "Dim light filters in from the south. Dusty passages run north and east.","You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.","North of you, the cave mount beckons.", "Dim light filters in from the south. Dusty passages run north and east.", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", "The narrow passage bends here from west to north. The smell of gold permeates the air.","Dark room full of old antiquities","Large room with massive dinner table ","Outdoor room with a large fountain and benches ","Massive kitchen filled with pots, pans and various cookingware ","Large room with a bed and sleeping quarters for any guests ","Largest room in the Castle with the kings and queens throne inside ","Small room filled with weapons to protect the Queen and King ", "you've entered the guards chamber, it is filled with beds for the King's guard and swords ","you've entered the buttery, it has a distinct smell of butter and other assorted foods ","the dimly lit chamber filled with lanterns and books, a recipe for disaster! ", "you've entered the darkest room in the castle, filled with ice and extremely insulated to keep everything inside ;) ","this is the most dangerous room in the house, most will not return once they enter... ","you have entered the gatehouse, over looking the border walls of the castle ","You have entered the most powerful room in the Castle, you can find weapons and armor in here! ","the gallery is the most historical room in the castle, all prior kings and queens are remembered here! ","this room over looks the edges of the Castle walls, great for protecting the Castle ","you've entered the stable house, filled with beautiful horses and their not so beautiful stench!","beware trespassers the sign says, this room is filled with traps! Tread carefully! "]  
         while room_count < num_rooms:
             if direction > 0 and x < size_x - 1:
                 room_direction = "e"
@@ -39,7 +39,7 @@ class World:
                 direction *= -1
             # Create a room in the given direction
             import random
-            num = random.choice(range(0,8))
+            num = random.choice(range(0,25))
             room = Room(title = room_titles[num], description = room_descriptions[num], x = x, y = y)
             room.save()
             self.grid[y][x] = room
